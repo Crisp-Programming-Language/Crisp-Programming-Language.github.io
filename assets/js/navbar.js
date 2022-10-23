e3fb9bb841dcf65ad1fa324ebd2ca04069820f40
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-10-23 15:52:55 -0400

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -3,3 +3,5 @@
       $("<a>").attr("href", "/about").addClass("link").text("About").appendTo("#navbar");
       $("<a>").attr("href", "/faq").addClass("link").text("FAQ").css("left", "35%").appendTo("#navbar");
       $("<a>").attr("href", "/download").addClass("link").text("Download").css("left", "45%").appendTo("#navbar");
+      $("<a>").attr("href", "https://github.com/Crisp-Programming-Language").text("GitHub").css("left", "63%").addClass("link").appendTo("#navbar");
+      $("<a>").attr("href", "https://scratch.mit.edu/discuss/topic/619355/").text("Jobs").addClass("link").css("left", "78%").appendTo("#navbar");
