67a6a404139fd80634aff16dc0890195f0a9224d
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-10-24 15:42:35 -0400

    Update and rename img-fade.js to home.js

---
+++ assets/js/home.js
@@ -0,0 +1,19 @@
+    var image = 1;
+    const src = ["https://raw.githubusercontent.com/Crisp-Programming-Language/Crisp-Programming-Language.github.io/main/assets/images/light-mode.png", "https://raw.githubusercontent.com/Crisp-Programming-Language/Crisp-Programming-Language.github.io/main/assets/images/dark-mode.png"];
+    const changeImg = function () {
+      image = 1;
+      $("#main-disp").show();
+      var $img = $("<img>").attr("src", src[image-1]).css("z-index", "4").hide().prependTo(".main-idea .img")
+      $img.fadeIn(2000);
+      $("#main-disp").fadeOut(2000, function(){
+            image = 2;
+          $("#main-disp").attr("src", src[image-1]).css("z-index", "5");
+          window.setTimeout(function() {
+              $("#main-disp").fadeIn(2000);
+              $img.fadeOut(2000, function() {
+                $img.remove();
+              }); 
+              }, 4000); 
+      });
+    }
+    window.setInterval(changeImg, 10000);
