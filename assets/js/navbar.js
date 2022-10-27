f07735058b845befa387a4238fafbb5a9763bd00
Indiana__Jones <104451887+IndianaJones-Programmer@users.noreply.github.com>
2022-10-27 19:06:19 +0200

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,5 @@
       $("<a>").attr("href", "/").appendTo("#navbar");
-      $("<img>").attr("src", "/assets/images/logo-a.png").attr("height", "100").appendTo("#navbar a");
+      $("<img>").attr("src", "/assets/images/crisp-logo.png").attr("height", "100").appendTo("#navbar a");
       $("<a>").attr("href", "/about").addClass("link").text("About").appendTo("#navbar");
       $("<a>").attr("href", "/faq").addClass("link").text("FAQ").css("left", "35%").appendTo("#navbar");
       $("<a>").attr("href", "/download").addClass("link").text("Download").css("left", "45%").appendTo("#navbar");
